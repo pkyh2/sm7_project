31d901d3157d10a85af17a14602fd34782bf16fa
pkyh2 <pkyh1@naver.com>
2022-03-20 22:23:27 +0900

    add lab28_ip

--- sm7/settings.py
+++ sm7/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^-7f+6)$(9l0lipz8+jf+im-f0=)1+5(ae)&mopz#jze2%b2$*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.34.95.117', '127.0.0.1', '10.0.2.2']
+ALLOWED_HOSTS = ['3.34.95.117', '127.0.0.1', '10.0.2.2', '35.77.144.191']
 
 
 # Application definition
